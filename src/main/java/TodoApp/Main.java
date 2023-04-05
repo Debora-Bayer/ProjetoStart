3e61167c6283626a55f062c5a83b510285ef877b
Debora Bayer <deborabbayer@hotmail.com>
2023-04-05 16:59:15 -0300

    primeiro commit

---
+++ src/main/java/TodoApp/Main.java
@@ -0,0 +1,31 @@
+package TodoApp;
+
+/**
+ *
+ * @author debora
+ */
+
+import controller.ProjectController;
+import java.sql.Connection;
+import java.util.List;
+import model.Project;
+import util.ConnectionFactory;
+
+public class Main{
+    public static void main(String[] args) {
+        ProjectController projectController = new ProjectController();;
+        Project project = new Project();
+        project.setName("Projeto Teste");
+        project.setDescription("description");
+        projectController.save(project);
+        
+        /*project.setName("Novo nome do projeto");
+        projectController.update(project);
+        
+        List<Project> projects = projectController.getAll();
+        System.out.println("Total de projetos = " + projects.size());
+       */
+        
+    }
+}
+
